cb2c68636d9201af78d5a3ac428d24f74697a606
minjae1025 <kjt081025@gmail.com>
2026-04-07 15:26:20 +0900

    TodoListApp, HTML, classNAme, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,29 +1,28 @@
 import { useState } from 'react'
+import './todolist.css'
 // import './App.css'
 
 function TodoListApp() {
     return (
-        <div>
-            <h1>Todo List</h1>
-            <form>
-                <input type="text" name="" id="" placeholder='할 일을 입력하세요' />
-                <button>todo</button>
+        <div className='todo'>
+            <h1 className='todo__title'>Todo List</h1>
+            <form className='todo__form'>
+                <input type="text" className='todo__input' name="" id="" placeholder='할 일을 입력하세요' />
+                <button className='todo__button todo__button--add'>add</button>
             </form>
-            <div>
-                <ul>
-                    <li>
-                        <input type="checkbox" name="" id="ck-1" />
-                        <label htmlFor="ck-1">놀기</label>
-                        <button>✏️</button>
-                        <button>❌</button>
-                    </li>
-                    <li>
-                        <input type="checkbox" name="" id="ck-2" />
-                        <label htmlFor="ck-2">집가기</label>
-                        <button>✏️</button>
-                        <button>❌</button>
-                    </li>
-                </ul>
+            <div className='todo__list'>
+                <li className='todo__item todo__complete'>
+                    <input type="checkbox" name="" id="ck-1" className='todo__check' />
+                    <label htmlFor="ck-1" className='todo__label'>놀기</label>
+                    <button className='todo__button todo__button--edit'>✏️</button>
+                    <button className='todo__button todo__button--delete'>❌</button>
+                </li>
+                <li>
+                    <input type="checkbox" name="" id="ck-2" />
+                    <label htmlFor="ck-2">집가기</label>
+                    <button>✏️</button>
+                    <button>❌</button>
+                </li>
             </div>
         </div>
     )
