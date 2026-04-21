5aed0048d5b528b3ed58cb34a8dc8124542c0f57
minjae1025 <kjt081025@gmail.com>
2026-04-21 15:41:31 +0900

    TodoListApp 끝 🔥

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ npm create vite@latest .
   * `setCount(() => set함수());`
   * `setCount(() => set함수(이전state => 이전state + 1));`
 ## Todo List App
+- React Component 분리
+- for -> htmlFor, class -> className
+- props
+- `<input id={id} value={} />`, `<label htmlFor={id} />`
+- onChange
+- 구조 분해 할당
+- `...연산자`
+- `<form onSubmit={} />`
+- `덩어리.map()`
+- `<TodoItem key={} />`
+- `const handleEvent = (event) => {}`
+- `{조건식 ? 참 : 거짓}`
+- `{조건식 && 참}`
+- `{!조건식 && 거짓}`
+- onKeyDown
+- LocalStorage, `useEffect()`
--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './todolist.css'
 // import Button from './components/Button'
 // import TodoItemEmpty from './components/TodoItemEmpty'
@@ -7,7 +8,6 @@ import TodoAdder from './components/TodoAdder.jsx'
 // import TodoItem from './components/TodoItem.jsx'
 import TodoList from './components/TodoList.jsx'
 // import './App.css'
-import { useEffect, useState } from 'react';
 
 class Todo {
     constructor(text) {
@@ -17,26 +17,36 @@ class Todo {
     }
 }
 
+const TODOS_STORAGE_KEY = 'todos';
 function TodoListApp() {
-    const [todos, setTodos] = useState([]); //todos는 현재 투 두 값, setTodos는 투 두 값을 업데이트하는 함수
+    const initTodos = () => {
+        const savedTodos = localStorage.getItem(TODOS_STORAGE_KEY);
+        return savedTodos ? JSON.parse(savedTodos) : [];
+    };
+    
+    const [todos, setTodos] = useState(initTodos); //todos는 현재 투 두 값, setTodos는 투 두 값을 업데이트하는 함수
+
+    useEffect(() => {
+        localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
+
+
     const addTodo = (text) => setTodos((todos) => [...todos, new Todo(text)]);
 
     const toggleTodo = (id) => {
-        setTodos((todos) => 
+        setTodos((todos) =>
             todos.map((todo) =>
-                todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo
+                todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
             )
         );
     }
 
-    const deleteTodo = (id) => {
-        setTodos((todos) => todos.filter((todo) => todo.id !== id));
-    }
+    const deleteTodo = (id) => setTodos((todos) => todos.filter((todo) => todo.id !== id));
 
     const editTodo = (id, editText) => {
         setTodos((todos) =>
             todos.map((todo) =>
-                todo.id === id ? {...todo, text: editText} : todo
+                todo.id === id ? { ...todo, text: editText } : todo
             )
         );
     }
